367b965557a289b616b9e3aae2b215b8dc1efd65
Carl Williams <carl.williams@betfred.com>
2016-05-03 18:16:54 +0100

    Entries saving

---
+++ HostsHandler/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace HostsHandler
+{
+    public class Helper 
+    {
+        public static string footerFormat = "#end of {0}";
+
+        public static string test() { return ""; }
+
+        public static Match getIPMatchFromString(string inputString)
+        {
+            return Regex.Match(inputString, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+        }
+
+    }
+}
